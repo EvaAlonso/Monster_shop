970b7cf647fbc3939d191090b6ca786a389eb5e1
Eva Alonso <formacio@BARCELO-GS58GHB.formacio.local>
2024-12-20 09:14:17 +0100

    initial commit

---
+++ src/test/java/com/bootcam/Monster_shop/MonsterShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcam.Monster_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
